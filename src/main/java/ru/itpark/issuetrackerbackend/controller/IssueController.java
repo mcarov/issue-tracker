73093774bf05e1458205e5f21e63575212e6d0b3
mcarov <mcarov@localhost>
2019-05-31 15:10:40 +0300

    Fixed #3.

--- src/main/java/ru/itpark/issuetrackerbackend/controller/IssueController.java
+++ src/main/java/ru/itpark/issuetrackerbackend/controller/IssueController.java
@@ -9,31 +9,32 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class IssueController {
     private final IssueService issueService;
 
-    @GetMapping("/api/issues")
+    @GetMapping("/issues")
     public List<Issue> getIssues() {
         return issueService.getIssues();
     }
 
-    @GetMapping("api/search/issues")
+    @GetMapping("/search/issues")
     public List<Issue> searchForIssues(@RequestParam String q) {
         return issueService.searchForIssues(q);
     }
 
-    @GetMapping("/api/issues/{id}")
+    @GetMapping("/issues/{id}")
     public Issue getIssueById(@PathVariable long id) {
         return issueService.getIssueById(id);
     }
 
-    @PostMapping("/api/issues")
+    @PostMapping("/issues")
     public void saveIssue(@RequestBody Issue issue) {
         issueService.saveIssue(issue);
     }
 
-    @DeleteMapping("/api/issues/{id}")
+    @DeleteMapping("/issues/{id}")
     public void removeIssue(@PathVariable long id) {
         issueService.removeIssueById(id);
     }
